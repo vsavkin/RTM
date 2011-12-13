0c173325270ed680164f7f56e2a4541c0b4c53d5
Victor Savkin <victors@nulogy.com>
2011-12-13 17:51:22 -0500

    adds tests for command reader

--- lib/rtm/command_reader.rb
+++ lib/rtm/command_reader.rb
@@ -2,6 +2,8 @@ module RTM
   class CommandReader
     def next_command
       #line = STDIN.readline
+      #return nil if line == 'exit'
+      #line
     end
   end
 end
--- tests/spec_helper.rb
+++ tests/spec_helper.rb
@@ -1,1 +1,2 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/rtm/application.rb')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/rtm/command_reader.rb')
